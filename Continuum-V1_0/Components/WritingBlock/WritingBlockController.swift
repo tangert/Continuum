636540607843ecd2457b3e6ed8704773908037a4
tangert <tyler@angert.com>
2018-07-24 06:53:30 -0400

    initial commit

---
+++ Continuum-V1_0/Components/WritingBlock/WritingBlockController.swift
@@ -0,0 +1,18 @@
+//
+//  WritingBlockController.swift
+//  Continuum-V1_0
+//
+//  Created by Tyler Angert on 7/23/18.
+//  Copyright © 2018 Tyler Angert. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WritingBlockController: UITableViewCell {
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var timeButton: UIButton!
+    @IBOutlet weak var locationButton: UIButton!
+    
+}
---
+++ Continuum-V1_0/Features/Content Production/ContentViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Continuum-V1_0
+//
+//  Created by Tyler Angert on 7/23/18.
+//  Copyright © 2018 Tyler Angert. All rights reserved.
+//
+
+import UIKit
+
+var items = [5,3]
+
+class ContentViewController: UIViewController {
+
+    // MARK: IB Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: View lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Tableview delegates
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Initial TableView setup
+        tableView.estimatedRowHeight = 100
+        tableView.tableFooterView = UIView(frame: .zero) // removes the extra cells
+    }
+}
+
+// MARK: TableView delegates
+extension ContentViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // FIXME: this will be updated as the author creates more text cells
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WritingBlock", for: indexPath) as! WritingBlockController
+        
+        cell.textView.delegate = self
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
+}
+
+// FIXME: Write some code to be able to "continue deletion" from one tableview cell to the next
+
+// MARK: TextView delegate for each cell
+extension ContentViewController: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+            self.tableView.beginUpdates()
+            self.tableView.endUpdates()
+    }
+    
+}
